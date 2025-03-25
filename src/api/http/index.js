fefdbabd56d639c28882866bc23b2e02d1dd9ec0
Vitalii Karpiv <vitalia.karpiv@gmail.com>
2025-03-25 19:31:55 +0200

    Set prod server URI

--- src/api/http/index.js
+++ src/api/http/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = `http://localhost:3000`
+export const API_URL = `http://ec2-54-211-80-95.compute-1.amazonaws.com`
 
 const $api = axios.create({
     withCredentials: true,
