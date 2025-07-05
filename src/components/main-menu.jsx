73e237920583ddc49ea95ccf74bbf0e2802908e1
Vitalii Karpiv <vitalia.karpiv@gmail.com>
2025-07-05 21:45:15 +0300

    Update menu component

--- src/components/main-menu.jsx
+++ src/components/main-menu.jsx
@@ -1,50 +1,44 @@
 import { Menu } from "antd";
-import { useNavigate } from "react-router-dom";
+import {Link} from "react-router-dom";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faBox, faCompass, faLaptop, faShoppingCart, faMoneyBill, faWarehouse} from '@fortawesome/free-solid-svg-icons'
 
 export default function MainMenu() {
-    const navigate = useNavigate();
     const items = [
         {
             key: 'dashboard',
-            label: 'Dashboard',
+            label: <Link to={"dashboard"}>Dashboard</Link>,
             icon: <FontAwesomeIcon icon={faCompass} />
         },
         {
             key: 'orders',
-            label: 'Orders',
+            label: <Link to={"orders"}>Orders</Link>,
             icon: <FontAwesomeIcon icon={faBox} />
         },
         {
             key: 'laptops',
-            label: 'Laptops',
+            label: <Link to={"laptops"}>Laptops</Link>,
             icon: <FontAwesomeIcon icon={faLaptop} />
         },
         {
             key: 'sales',
-            label: 'Sales',
+            label: <Link to={"sales"}>Sales</Link>,
             icon: <FontAwesomeIcon icon={faShoppingCart} />
         },
         {
             key: 'finances',
-            label: 'Finances',
+            label: <Link to={"finances"}>Finances</Link>,
             icon: <FontAwesomeIcon icon={faMoneyBill} />
         },
         {
             key: 'inventory',
-            label: 'Inventory',
+            label: <Link to={"inventory"}>Inventory</Link>,
             icon: <FontAwesomeIcon icon={faWarehouse} />
         }
     ];
 
-    const onClick = (e) => {
-        navigate(`/${e.key}`)
-    };
-
 
     return <Menu
-        onClick={onClick}
         items={items}
         style={{
             boxShadow: "none",
