2ed508c1399479cadfcb0da819146fb3262bc5ab
Vitalii Karpiv <vitalia.karpiv@gmail.com>
2025-11-13 21:24:20 +0200

    Add connection to cloud api

--- src/api/http/index.js
+++ src/api/http/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const API_URL = `http://localhost:3000`;
+// export const API_URL = `https://api.kuzcocrm.com`;
 
 const $api = axios.create({
   withCredentials: true,
